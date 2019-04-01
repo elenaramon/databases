fe70944a8e091262314de25f5f95add61a26265a
Elena Ramon <ramon.elena97@gmail.com>
2019-04-01 14:31:31 +0200

    Revert "Esercizi svolti 1-2"
    This reverts commit 3862953d5fa9eb991d3922bbaabd0c06bad2727e.

--- prova.sql
+++
@@ -1,8 +0,0 @@
--- Esempio di file comando SQL
--- cancello la tabella
-DROP TABLE pippo;
--- creo la tabella
-CREATE TABLE pippo (
-    id INTEGER PRIMARY KEY,
-    nome VARCHAR NOT NULL UNIQUE
-);
